bdfb06fb82acd449c1be0a4b52ec87f5b8227c12
Salaruddin <salaruddinjalal@gmail.com>
2024-10-11 19:11:55 +0430

    RegisterPage: on importing InputFiled, the file name first letter "i" capitalized

--- src/pages/public/RegisterPage.tsx
+++ src/pages/public/RegisterPage.tsx
@@ -1,7 +1,7 @@
 import * as Yup from 'yup';
 import { useForm } from 'react-hook-form';
 import { IRegisterDto } from '../../types/auth.types';
-import InputField from '../../components/general/inputField';
+import InputField from '../../components/general/InputField';
 import { yupResolver } from '@hookform/resolvers/yup';
 import useAuth from '../../hooks/useAuth.hook';
 import Button from '../../components/general/Button';
